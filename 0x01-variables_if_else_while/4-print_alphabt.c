dde5ac54c2429410dcd1f96430caa4d9113a7f05
christianwondeson <christianwondeson22@gmail.com>
2022-03-11 13:07:01 -0800

    commit alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point and remove
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
