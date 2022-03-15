a90033c93cab8548d3e63ee6bc387db303d6d212
christianwondeson <christianwondeson22@gmail.com>
2022-03-15 05:43:39 -0700

    display putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,31 @@
 # include <stdio.h>
+# include "main.h"
 /**
  * main- Entery point
  *
  * Return: Always 0 (success)
  */
-# include "main.h"
-
 int main(void)
 {
-	printf("%s\n", disp);
+	char print[9];
+	int i = 0;
+
+	print[0] = '_';
+	print[1] = 'p';
+	print[2] = 'u';
+	print[3] = 't';
+	print[4] = 'c';
+	print[5] = 'h';
+	print[6] = 'a';
+	print[7] = 'r';
+	print[8] = '\n';
+	for (i = 0; i <= 8; i++)
+	{
+		if (print[i] != EOF)
+
+		{
+			_putchar(print[i]);
+		}
+	}
 	return (0);
 }
