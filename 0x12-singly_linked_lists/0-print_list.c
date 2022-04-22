9c61ee3c5b3bd29c3b36e51748138fe61b5342f5
christianwondeson <christianwondeson22@gmail.com>
2022-04-22 14:34:14 -0700

    task-0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,56 +1,24 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@christianwondeson 
-girumtim
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-alx-low_level_programming/0x12-singly_linked_lists/0-print_list.c
-@girumtim
-girumtim mandatory
-Latest commit 5aa5f61 23 hours ago
- History
- 1 contributor
-28 lines (22 sloc)  387 Bytes
-   
 #include "lists.h"
 #include <stdio.h>
 
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
+ * print_list - print elements of linkedlist
+ * @h: linked list
  *
- * Return: The number of nodes in h.
+ * Return: number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t nbr_nodes = 0;
+	const list_t *current = h;
 
-	while (h)
+	while (current != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
-		h = h->next;
+		printf("[%d] %s\n", current->len,
+		current->str != NULL ? current->str : "(nil)");
+		current = current->next;
+		nbr_nodes++;
 	}
-
-	return (nodes);
+	return (nbr_nodes);
 }
