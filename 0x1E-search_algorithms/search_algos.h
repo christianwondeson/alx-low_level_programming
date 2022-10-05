715cf5e5c85c3a2f4da57e6a2df1589b955ff0ca
chris_wendi <72356458+christianwondeson@users.noreply.github.com>
2022-10-05 20:50:47 +0300

    Add files via upload

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, int start, int end);
+
+#endif
